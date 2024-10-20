84a7593d4817d7c48dd31620c7fd849c03c3051c
rushaan1 <cocrushaan3@gmail.com>
2024-10-20 20:43:37 +0530

    Navigation drawer almost complete, storage viewer left

--- Cloud Storage Platform/AngularApplication/CloudStoragePlatformUI/src/app/app.component.ts
+++ Cloud Storage Platform/AngularApplication/CloudStoragePlatformUI/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.miniToggle(false);
+  }
+
   title = 'CloudStoragePlatformUI';
+  miniset:boolean = false;
+
+  miniToggle(event:boolean){
+    this.miniset = event;
+    const navDrawer = document.getElementsByClassName("navigation-drawer")[0] as HTMLElement;
+    navDrawer.style.width = event ? "50px" : "321px";
+  }
 }
--- Cloud Storage Platform/AngularApplication/CloudStoragePlatformUI/src/app/navigation-drawer/navigation-drawer.component.ts
+++ Cloud Storage Platform/AngularApplication/CloudStoragePlatformUI/src/app/navigation-drawer/navigation-drawer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,10 +6,16 @@ import { Router } from '@angular/router';
   templateUrl: './navigation-drawer.component.html',
   styleUrl: './navigation-drawer.component.css'
 })
-export class NavigationDrawerComponent {
+export class NavigationDrawerComponent implements OnInit {
+  @Output() miniSet = new EventEmitter<boolean>();
   selectedTypeItems: string[] = [];
   constructor(private router: Router) {
 
+  }
+  ngOnInit(): void {
+    const miniDrawer = document.getElementsByClassName("mini-drawer")[0] as HTMLElement;
+    miniDrawer.style.translate = "-100%";
+    console.log(miniDrawer.style.translate);
   }
 
   hoverTransitionTrigger(event: MouseEvent) {
@@ -18,16 +24,14 @@ export class NavigationDrawerComponent {
       const inputElement = targetElement.querySelector('input[type="checkbox"]') as HTMLInputElement;
       const aElement = targetElement.querySelector("a") as HTMLAnchorElement;
       const iElement = aElement.getElementsByTagName("i")[0] as HTMLElement;
-
       if (inputElement) {
         inputElement.style.visibility = "visible";
         inputElement.style.marginLeft = "15px";
       }
-      aElement.style.paddingLeft = "21px"
-      if (aElement && this.isSelected(targetElement)==false) {
-        // aElement.style.backgroundColor = "antiquewhite";
-        // targetElement.style.backgroundColor = "antiquewhite";
-        // iElement.style.backgroundColor = "antiquewhite";
+      if (targetElement.classList.contains("m-cat-tab") == false) {
+        aElement.style.paddingLeft = "21px"
+      }
+      if (aElement && this.isSelected(targetElement) == false) {
         this.setBackgroundColors(aElement, targetElement, iElement, "antiquewhite");
       }
     }
@@ -39,7 +43,7 @@ export class NavigationDrawerComponent {
       const inputElement = targetElement.querySelector('input[type="checkbox"]') as HTMLInputElement;
       const aElement = targetElement.querySelector("a") as HTMLAnchorElement;
       const iElement = aElement.getElementsByTagName("i")[0] as HTMLElement;
-
+      console.log(targetElement);
       if (inputElement) {
         inputElement.style.visibility = "hidden";
         inputElement.style.marginLeft = "0";
@@ -47,10 +51,7 @@ export class NavigationDrawerComponent {
       if (aElement) {
         aElement.style.paddingLeft = "0"
 
-        if (this.isSelected(targetElement)==false) {
-          // aElement.style.backgroundColor = "";
-          // iElement.style.backgroundColor = "";
-          // targetElement.style.backgroundColor = "";
+        if (this.isSelected(targetElement) == false) {
           this.setBackgroundColors(aElement, targetElement, iElement, "");
         }
       }
@@ -58,53 +59,68 @@ export class NavigationDrawerComponent {
   }
 
   typeItemSelected(event: MouseEvent, liIndex: number) {
-    const checkbox = document.getElementById(`${liIndex}-NavCheckbox`) as HTMLInputElement;
-    const lis = document.getElementsByClassName("bottom-category")[0].getElementsByTagName("li");
+    var checkbox = document.getElementById(`${liIndex}-NavCheckbox`) as HTMLInputElement;
+    const lis = document.getElementsByClassName("maindrawer-bottom")[0].getElementsByTagName("li");
     const li = lis[liIndex];
     const aLi = li.getElementsByTagName("a")[0] as HTMLAnchorElement; // corresponding anchor tag
     const iElement = aLi.getElementsByTagName("i")[0] as HTMLElement;
     const type = li.textContent?.trim();
 
+    this.itemSelection(li, type, aLi, iElement, false, checkbox);
+    this.hoverTransitionTrigger(event);
+    console.log(this.selectedTypeItems);
+  }
+
+  itemSelection(li: HTMLElement, type: string | undefined, aLi: HTMLElement, iElement: HTMLElement, mini: boolean, checkbox: any | undefined) {
     if (li.style.backgroundColor != "bisque") {
       if (type) {
         this.selectedTypeItems.push(type);
       }
-      checkbox.checked = true;
-
-      // li.style.backgroundColor = "bisque";
-      // aLi.style.backgroundColor = "bisque";
-      // iElement.style.backgroundColor = "bisque";
+      if (!mini) {
+        checkbox.checked = true;
+      }
       this.setBackgroundColors(li, aLi, iElement, "bisque");
     }
+
     else if (li.style.backgroundColor == "bisque") {
       if (type) {
         this.selectedTypeItems.splice(this.selectedTypeItems.indexOf(type), 1);
       }
-      li.style.backgroundColor = "";
-      checkbox.checked = false;
-      // aLi.style.backgroundColor = "antiquewhite";
-      // iElement.style.backgroundColor = "antiquewhite";
-      // li.style.backgroundColor = "antiquewhite";
+      if (!mini) {
+        checkbox.checked = false;
+      }
       this.setBackgroundColors(aLi, li, iElement, "antiquewhite");
-      
+      li.style.backgroundColor = "";
     }
+  }
+
+  typeItemSelectedMini(event: MouseEvent, name: string, liIndex: number) {
+    const lis = document.getElementsByClassName("m-bottom-cat")[0].getElementsByTagName("li");
+    const li = lis[liIndex];
+    const aLi = li.getElementsByTagName("a")[0] as HTMLAnchorElement; // corresponding anchor tag
+    const iElement = aLi.getElementsByTagName("i")[0] as HTMLElement;
+
+    this.itemSelection(li, name, aLi, iElement, true, undefined);
+    this.hoverTransitionTrigger(event);
     console.log(this.selectedTypeItems);
   }
 
+
   isSelected(targetElement: HTMLElement): any {
-    const content = targetElement.textContent?.trim();
-    if (content != null) {
-      const index = this.selectedTypeItems.indexOf(content);
-      if (index != -1) {
-        return true;
-      }
-      return false
+    var content = targetElement.textContent?.trim();
+    // console.log(targetElement);
+    if (content == null || content == undefined || content == "") {
+      content = targetElement.id;
     }
-    return Error("Null content");
+    const index = this.selectedTypeItems.indexOf(content);
+    if (index != -1) {
+      return true;
+    }
+    return false
   }
 
-  setBackgroundColors(first:HTMLElement, second:HTMLElement, third:HTMLElement, value:string){
-    if (value==""){
+  setBackgroundColors(first: HTMLElement, second: HTMLElement, third: HTMLElement, value: string) {
+    if (value == "") {
       value = "transparent"
     }
     first.style.backgroundColor = value;
@@ -112,19 +128,79 @@ export class NavigationDrawerComponent {
     third.style.backgroundColor = value;
   }
 
-  topCategoryLiHover(event:MouseEvent){
+  topCategoryLiHover(event: MouseEvent) {
     const targetElement = event.currentTarget as HTMLElement;
     const aElement = targetElement.querySelector("a") as HTMLAnchorElement;
     const iElement = aElement.getElementsByTagName("i")[0] as HTMLElement;
 
-    this.setBackgroundColors(targetElement,aElement,iElement,"antiquewhite");
+    this.setBackgroundColors(targetElement, aElement, iElement, "antiquewhite");
   }
 
-  topCategoryLiHoverAway(event:MouseEvent){
+  topCategoryLiHoverAway(event: MouseEvent) {
     const targetElement = event.currentTarget as HTMLElement;
     const aElement = targetElement.querySelector("a") as HTMLAnchorElement;
     const iElement = aElement.getElementsByTagName("i")[0] as HTMLElement;
 
-    this.setBackgroundColors(targetElement,aElement,iElement,"");
+    this.setBackgroundColors(targetElement, aElement, iElement, "");
+  }
+
+  visibilityToggle(navDrawer: HTMLElement, visibility: string) {
+    navDrawer.style.visibility = visibility;
+    (document.getElementsByClassName("maindrawer-top")[0] as HTMLElement).style.visibility = visibility;
+    (document.getElementsByClassName("maindrawer-bottom")[0] as HTMLElement).style.visibility = visibility;
+  }
+
+  drawerClick() {
+    const navDrawer = document.getElementsByClassName("drawer")[0] as HTMLElement;
+    const miniDrawer = document.getElementsByClassName("mini-drawer")[0] as HTMLElement;
+    setTimeout(() => {
+      this.miniSet.emit(true);
+      this.visibilityToggle(navDrawer, "hidden");
+    }, 700);
+    miniDrawer.style.visibility = "visible";
+    navDrawer.style.translate = "-300%";
+    miniDrawer.style.translate = "0%"
+
+    const miniTabs = document.getElementsByClassName("m-cat-tab");
+
+    this.retainingSelection(miniTabs);
+  }
+
+  miniDrawerClick() {
+    const navDrawer = document.getElementsByClassName("drawer")[0] as HTMLElement;
+    const miniDrawer = document.getElementsByClassName("mini-drawer")[0] as HTMLElement;
+
+    this.visibilityToggle(navDrawer, "visible");
+    navDrawer.style.translate = "0%";
+    miniDrawer.style.translate = "-100%"
+    miniDrawer.style.visibility = "hidden";
+
+    this.miniSet.emit(false);
+
+    this.retainingSelection(document.getElementsByClassName("category-tab"));
+  }
+
+  retainingSelection(typeOptions: any) {
+    for (let i = 0; i < typeOptions.length; i++) {
+      let tab = typeOptions[i] as HTMLElement;
+
+      const aElement = tab.querySelector("a") as HTMLAnchorElement;
+      const iElement = aElement.getElementsByTagName("i")[0] as HTMLElement;
+      const input = tab.getElementsByTagName("input")[0];
+
+      if (this.selectedTypeItems.indexOf(tab.id) != -1) {
+        this.setBackgroundColors(tab, aElement, iElement, "bisque");
+
+        if (input != undefined && input != null) {
+          input.checked = true;
+        }
+      }
+      else {
+        this.setBackgroundColors(tab, aElement, iElement, "");
+        if (input != undefined && input != null) {
+          input.checked = false;
+        }
+      }
+    }
   }
 }
