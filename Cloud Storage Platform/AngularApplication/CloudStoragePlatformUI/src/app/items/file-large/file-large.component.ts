dd20fc6b50f5417d6ddd8f01e7c9f09636ecf1da
rushaan1 <cocrushaan3@gmail.com>
2024-10-25 16:05:34 +0530

    Completed file rename feature

--- Cloud Storage Platform/AngularApplication/CloudStoragePlatformUI/src/app/items/file-large/file-large.component.ts
+++ Cloud Storage Platform/AngularApplication/CloudStoragePlatformUI/src/app/items/file-large/file-large.component.ts
@@ -8,47 +8,66 @@ import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 export class FileLargeComponent implements OnInit {
   @Input() type: string = "";
   @Input() name: string = "";
-  @ViewChild("fileNameInput") fileNameInput!: ElementRef;
-  originalName="";
+  @ViewChild("fileNameInput", { static: false }) fileNameInput?: ElementRef<HTMLInputElement>;
+  originalName = "";
   fileOptionsShouldBeVisible = false;
   renaming = false;
 
   ngOnInit(): void {
     this.originalName = this.name;
-    if (this.name.length>=32){
+    this.nameResizing();
+  }
+
+  nameResizing(){
+    if (this.name.length >= 32) {
       let portionToBeExcluded = this.name.slice(32, this.name.length);
-      this.name = this.name.replace(portionToBeExcluded, "")+"...";
+      this.name = this.name.replace(portionToBeExcluded, "") + "...";
     }
   }
 
-  expandOptions(){
+  expandOptions() {
     const menu = document.getElementsByClassName("file-options-menu")[0] as HTMLElement;
-    if (this.fileOptionsShouldBeVisible == false){
+    if (this.fileOptionsShouldBeVisible == false) {
       menu.style.visibility = "visible";
       menu.style.height = "200px";
       this.fileOptionsShouldBeVisible = true;
       this.applyMargin(menu.querySelectorAll("div"));
     }
-    else{
+    else {
       menu.style.height = "0";
       this.fileOptionsShouldBeVisible = false;
-      setTimeout(()=>{
+      setTimeout(() => {
         menu.style.visibility = "hidden";
-      },800);
+      }, 800);
     }
   }
 
-  applyMargin(options:NodeListOf<HTMLDivElement>){
-    for (let i = 1; i<options.length; i++){
+  applyMargin(options: NodeListOf<HTMLDivElement>) {
+    for (let i = 1; i < options.length; i++) {
       console.log(options[i].textContent);
-      options[i].style.marginTop = `${31*i}px`;
+      options[i].style.marginTop = `${31 * i}px`;
     }
   }
 
-  renameEnter(){
-    if (!this.renameEnter){
+  renameEnter() {
+    if (!this.renameEnter) {
       return;
     }
+    if (this.fileNameInput?.nativeElement){
+      this.originalName = this.fileNameInput?.nativeElement?.value;
+      this.name = this.originalName;
+      this.nameResizing();
+      console.log("Name updated?"); 
+    }
     this.renaming = false;
+  }
+
+  setupInput() {
+    this.renaming = this.renaming ? false : true;
+    this.expandOptions();
+    setTimeout(() => {
+      this.fileNameInput?.nativeElement?.focus();
+      this.fileNameInput?.nativeElement?.select();
+    }, 50);
   }
 }
