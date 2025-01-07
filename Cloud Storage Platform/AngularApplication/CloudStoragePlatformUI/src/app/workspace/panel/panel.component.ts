04e3fc805c555a5b722ce2515a9069c45b9044f6
rushaan1 <cocrushaan3@gmail.com>
2025-01-08 01:05:07 +0530

    Added pfp dropdown for showing first name with info of clicking to open account settings.

--- Cloud Storage Platform/AngularApplication/CloudStoragePlatformUI/src/app/workspace/panel/panel.component.ts
+++ Cloud Storage Platform/AngularApplication/CloudStoragePlatformUI/src/app/workspace/panel/panel.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FormControl, Validators} from "@angular/forms";
 import {EventService} from "../../services/event-service.service";
 import {invalidCharacter, invalidFileNameChars} from "../../CustomValidators";
@@ -8,11 +8,35 @@ import {invalidCharacter, invalidFileNameChars} from "../../CustomValidators";
   templateUrl: './panel.component.html',
   styleUrl: './panel.component.css'
 })
-export class PanelComponent {
+export class PanelComponent implements OnInit, AfterViewChecked {
   @ViewChild('searchDiv') searchDiv!: ElementRef<HTMLInputElement>;
-  searchFormControl = new FormControl("", [Validators.required, Validators.pattern(/\S/), invalidCharacter]);
+  @ViewChild('pfpDropdown') pfpDropdownDiv!: ElementRef<HTMLDivElement>;
+  @ViewChild('panelMainFlex') panelMainFlex!: ElementRef<HTMLDivElement>;
+  @ViewChild('pfp') pfp!: ElementRef<HTMLDivElement>;
+  @ViewChild('pfpDropDownSpan') pfpDropDownSpan!: ElementRef<HTMLSpanElement>;
+  mouseHoveringOverPfp = false;
+  onlyHiText = false;
 
+  searchFormControl = new FormControl("", [Validators.required, Validators.pattern(/\S/), invalidCharacter]);
+  pfpDropdownShowing = false;
   constructor(public eventService:EventService){}
+
+  ngOnInit(){
+    this.showStartupWelcomeMsgWithPfpDropDown();
+  }
+
+  ngAfterViewChecked(){
+    if (this.pfpDropdownShowing && this.pfpDropdownDiv.nativeElement){
+      const rect = this.pfp.nativeElement.getBoundingClientRect();
+      this.pfpDropdownDiv.nativeElement.style.right = `${rect.top}px`;
+      if (this.onlyHiText){
+        this.pfpDropDownSpan.nativeElement.innerText = "Hi FirstName";
+      }
+      else{
+        this.pfpDropDownSpan.nativeElement.innerHTML = "Hi FirstName, <br> <b>Click to open account settings</b>";
+      }
+    }
+  }
 
   searchDarkenBorder(){
     this.searchDiv.nativeElement.style.borderColor = "black";
@@ -48,5 +72,24 @@ export class PanelComponent {
       }
       this.searchDiv.nativeElement.classList.add("red-search-border");
     }
+  }
+
+  showPfpDropdown(onlyHiText:boolean){
+    this.pfpDropdownShowing = true;
+    this.onlyHiText = onlyHiText;
+  }
+
+  hidePfpDropdown(){
+    this.pfpDropdownShowing = false;
+  }
+
+  showStartupWelcomeMsgWithPfpDropDown(){
+    this.showPfpDropdown(true);
+
+    setTimeout(() => {
+      if (!this.mouseHoveringOverPfp){
+        this.hidePfpDropdown();
+      }
+    }, 8000);
   }
 }
